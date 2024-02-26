e3549dffada5d8fc61a8360b6b84e716bec75b6f
Walter Huf <hufman@gmail.com>
2024-02-25 20:50:57 -0800

    Set model data on UI thread

--- app/src/main/java/io/bimmergestalt/headunit/rhmi/RHMIApplicationState.kt
+++ app/src/main/java/io/bimmergestalt/headunit/rhmi/RHMIApplicationState.kt
@@ -1,5 +1,7 @@
 package io.bimmergestalt.headunit.rhmi
 
+import android.os.Handler
+import android.os.Looper
 import androidx.compose.runtime.mutableStateMapOf
 import io.bimmergestalt.idriveconnectkit.rhmi.RHMIAction
 import io.bimmergestalt.idriveconnectkit.rhmi.RHMIApplication
@@ -10,6 +12,7 @@ import io.bimmergestalt.idriveconnectkit.rhmi.RHMIState
 
 
 class RHMIApplicationState(): RHMIApplication() {
+	private val handler = Handler(Looper.getMainLooper())
 	override val actions: MutableMap<Int, RHMIAction> = HashMap()
 	override val components: MutableMap<Int, RHMIComponent> = HashMap()
 	override val events: MutableMap<Int, RHMIEvent> = HashMap()
@@ -20,7 +23,9 @@ class RHMIApplicationState(): RHMIApplication() {
 	val propertyStates = HashMap<Int, MutableMap<Int, Any?>>().withDefault { mutableStateMapOf() }
 
 	override fun setModel(modelId: Int, value: Any) {
-		modelStates[modelId] = value
+		handler.post {
+			modelStates[modelId] = value
+		}
 	}
 	override fun getModel(modelId: Int): Any? {
 		return modelStates[modelId]
@@ -28,7 +33,9 @@ class RHMIApplicationState(): RHMIApplication() {
 
 
 	override fun setProperty(componentId: Int, propertyId: Int, value: Any?) {
-		propertyStates[componentId]!![propertyId] = value
+		handler.post {
+			propertyStates[componentId]!![propertyId] = value
+		}
 	}
 	override fun getProperty(componentId: Int, propertyId: Int): Any? = propertyStates[componentId]!![propertyId]
 
