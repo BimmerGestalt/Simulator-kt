582f34c9894c3a3bf1dc49784efcfcff98d70fa9
Walter Huf <hufman@gmail.com>
2024-03-02 15:38:29 -0800

    Implement Calendar states

---
+++ app/src/main/java/io/bimmergestalt/headunit/ui/components/Calendar.kt
@@ -0,0 +1,165 @@
+package io.bimmergestalt.headunit.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.bimmergestalt.headunit.ui.theme.ColorTheme
+import io.bimmergestalt.headunit.ui.theme.HeadunitktTheme
+import io.bimmergestalt.headunit.utils.padStart
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.isoDayNumber
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
+
+/**
+ * Contains much inspiration from https://github.com/boguszpawlowski/ComposeCalendar
+ */
+
+fun LocalDate.startOfWeekDay(firstDayOfWeek: DayOfWeek): LocalDate {
+	if (firstDayOfWeek.isoDayNumber <= dayOfWeek.isoDayNumber) {
+		return this.minus(dayOfWeek.isoDayNumber - firstDayOfWeek.isoDayNumber, DateTimeUnit.DAY)
+	} else {
+		return this.minus(dayOfWeek.isoDayNumber + 7 - firstDayOfWeek.isoDayNumber, DateTimeUnit.DAY)
+	}
+}
+
+@Composable
+fun CalendarMonth(modifier: Modifier = Modifier, selectedDate: LocalDate, firstDayOfWeek: DayOfWeek = DayOfWeek.MONDAY,
+                  highlightDay: (LocalDate) -> Boolean, onSelectedDate: (LocalDate) -> Unit, onClick: (LocalDate) -> Unit) {
+	val startOfMonth = LocalDate(year=selectedDate.year, monthNumber = selectedDate.monthNumber, dayOfMonth = 1)
+	val startOfWeek = startOfMonth.startOfWeekDay(firstDayOfWeek)
+	val firstDayDisplayed = if (startOfMonth.dayOfWeek == firstDayOfWeek) {
+		startOfWeek.minus(1, DateTimeUnit.WEEK) // make sure there's a blank week before
+	} else {
+		startOfWeek
+	}
+	val WEEK_DAY_NAMES = listOf("MO", "TU", "WE", "TH", "FR", "SA", "SU")
+
+	Table(modifier = modifier, columnCount = 8, rowCount = 7,
+		verticalAlignment = Alignment.Top, horizontalAlignment = Alignment.CenterHorizontally,
+		afterRow = { Divider() }) { columnIndex, rowIndex ->
+		if (rowIndex == 0) {
+			val heading = if (columnIndex == 0) { "WK" }
+			else {
+				WEEK_DAY_NAMES[firstDayDisplayed.plus(columnIndex - 1, DateTimeUnit.DAY).dayOfWeek.isoDayNumber - 1]
+			}
+			Text(heading,
+				modifier = Modifier.padding(6.dp),
+				style = MaterialTheme.typography.titleMedium,
+				color = MaterialTheme.colorScheme.secondary)
+		} else {
+			val cellDay = if (columnIndex > 0) firstDayDisplayed.plus((rowIndex - 1) * 7 + columnIndex - 1, DateTimeUnit.DAY) else null
+
+			val backgroundColor = if (cellDay == selectedDate) {
+				MaterialTheme.colorScheme.primary
+			} else null
+			val cellModifier = if (backgroundColor != null) {
+				Modifier.background(backgroundColor)
+			} else { Modifier }
+			val clickableCellModifier = if (cellDay != null) {
+				if (cellDay.month == selectedDate.month) {
+					if (highlightDay(cellDay)) {    // day has events to click into
+						cellModifier.clickable { onClick(cellDay) }
+					} else {    // just change the highlight
+						cellModifier.clickable { onSelectedDate(cellDay) }
+					}
+				} else {
+					cellModifier.clickable { onSelectedDate(cellDay) }
+				}
+			} else { cellModifier }
+
+			Column(modifier = clickableCellModifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+				if (columnIndex == 0) {
+					val weekDay = firstDayDisplayed.plus((rowIndex - 1) * 7, DateTimeUnit.DAY)
+					Text((weekDay.dayOfYear / 7 + 1).toString(),
+						style = MaterialTheme.typography.titleMedium,
+						color = MaterialTheme.colorScheme.secondary)
+				} else if (cellDay != null) {
+					val color = if (cellDay == selectedDate) {
+						MaterialTheme.colorScheme.onPrimary
+					} else {
+						if (cellDay.month == selectedDate.month) {MaterialTheme.colorScheme.primary} else {MaterialTheme.colorScheme.secondary}
+					}
+					Text(cellDay.dayOfMonth.toString(),
+						textAlign = TextAlign.Center,
+						style = MaterialTheme.typography.titleMedium,
+						color = color)
+					if (highlightDay(cellDay)) {
+						Image(painter = ColorPainter(MaterialTheme.colorScheme.tertiary),
+							contentDescription = null,
+							modifier = Modifier
+								.size(8.dp, 8.dp)
+								.clip(CircleShape))
+					} else {
+						Box(modifier = Modifier.size(8.dp, 8.dp))
+					}
+				}
+			}
+		}
+	}
+}
+
+data class CalendarEvent(val start: LocalTime, val end: LocalTime, val title: String)
+
+@Composable
+fun CalendarDayView(modifier: Modifier = Modifier,  events: List<CalendarEvent>, onClick: (CalendarEvent) -> Unit) {
+	LazyColumn(modifier = modifier) {
+		items(events) {
+			Column(Modifier.padding(4.dp).clickable { onClick(it) }) {
+				val startTime = "${it.start.hour.padStart(2, '0')}:${it.start.minute.padStart(2, '0')}"
+				val endTime = "${it.end.hour.padStart(2, '0')}:${it.end.minute.padStart(2, '0')}"
+				val time = "$startTime - $endTime"
+				Text(time, modifier = Modifier.padding(4.dp),
+					style = MaterialTheme.typography.bodyMedium, color = MaterialTheme.colorScheme.primary)
+				Text(it.title, modifier = Modifier.padding(4.dp),
+					style = MaterialTheme.typography.bodyMedium, color = MaterialTheme.colorScheme.tertiary)
+			}
+		}
+	}
+}
+
+@Preview
+@Composable
+fun Preview() {
+
+	HeadunitktTheme(colorTheme = ColorTheme.Lagoon,
+		darkTheme = isSystemInDarkTheme()) {
+		Surface {
+//			CalendarMonth(
+//				firstDayOfWeek = DayOfWeek.SUNDAY,
+//				selectedDate = LocalDate(2024, 2, 19),
+//				highlightDay = { it.dayOfYear % 9 == 0 },
+//				onSelectedDate = {}) {}
+			CalendarDayView(Modifier, listOf(
+				CalendarEvent(LocalTime(5, 0), LocalTime(5, 30), "Coffee"),
+				CalendarEvent(LocalTime(7, 0), LocalTime(8, 30), "Exercise"),
+				CalendarEvent(LocalTime(6, 0), LocalTime(6, 30), "Breakfast"),
+				CalendarEvent(LocalTime(10, 0), LocalTime(16, 0), "Work"),
+			)) {}
+		}
+	}
+}
---
+++ app/src/main/java/io/bimmergestalt/headunit/ui/screens/RHMICalendarState.kt
@@ -0,0 +1,133 @@
+package io.bimmergestalt.headunit.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.bimmergestalt.headunit.rhmi.RHMIDateUtils
+import io.bimmergestalt.headunit.ui.components.CalendarDayView
+import io.bimmergestalt.headunit.ui.components.CalendarEvent
+import io.bimmergestalt.headunit.ui.components.CalendarMonth
+import io.bimmergestalt.headunit.utils.asEtchInt
+import io.bimmergestalt.idriveconnectkit.rhmi.RHMIAction
+import io.bimmergestalt.idriveconnectkit.rhmi.RHMIComponent
+import io.bimmergestalt.idriveconnectkit.rhmi.RHMIModel
+import io.bimmergestalt.idriveconnectkit.rhmi.RHMIState
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format
+import kotlinx.datetime.format.MonthNames
+import kotlinx.datetime.format.char
+import kotlinx.datetime.toLocalDateTime
+
+fun LocalDate.asRhmiDate(): Int = RHMIDateUtils.convertToRhmiDate(this)
+fun Int.fromRhmiDate(): LocalDate = RHMIDateUtils.convertFromRhmiDate(this)
+
+data class CalendarMonthStateViewModel(
+	val onChangeAction: suspend (date: LocalDate) -> Unit,
+	val onAction: suspend (date: LocalDate) -> Unit,
+	val date: LocalDate,
+	val highlightList: List<LocalDate>
+)
+
+fun RHMIState.CalendarMonthState.viewModel(actionHandler: suspend (RHMIAction?, extra: Map<Int, Any>?) -> Unit): CalendarMonthStateViewModel {
+	val date = this.getDateModel()?.asRaIntModel()?.value
+		?.takeIf { it > LocalDate(1900, 1, 1).asRhmiDate() }?.fromRhmiDate() ?:
+		Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
+	val highlightListModel = this.getHighlightListModel()?.asRaListModel()?.value ?: RHMIModel.RaListModel.RHMIListConcrete(1)
+	val highlightList = highlightListModel.getWindow(0, highlightListModel.endIndex).mapNotNull { dataRow ->
+		dataRow.getOrNull(0)?.asEtchInt()?.let { LocalDate(date.year, date.monthNumber, it) }
+	}
+	return CalendarMonthStateViewModel(
+		onChangeAction = { actionHandler(this.getChangeAction(), mapOf(0 to it.asRhmiDate())) },
+		onAction = { actionHandler(this.getAction(), mapOf(0 to it.asRhmiDate())) },
+		date = date,
+		highlightList = highlightList
+	)
+}
+
+@Composable
+fun RHMICalendarMonthState(modifier: Modifier, state: CalendarMonthStateViewModel) {
+	val scope = rememberCoroutineScope()
+	var localDate by remember { mutableStateOf(state.date)}
+
+	CalendarMonth(
+		modifier = modifier,
+		selectedDate = localDate,
+		firstDayOfWeek = DayOfWeek.SUNDAY,
+		highlightDay = { state.highlightList.contains(it) },
+		onSelectedDate = {scope.launch { localDate = it; state.onChangeAction(it) }}
+	) {
+		scope.launch {
+			localDate = it
+			state.onAction(it)
+		}
+	}
+}
+
+
+fun LocalTime.asRhmiTime(): Int = RHMIDateUtils.convertToRhmiTime(this)
+fun Int.fromRhmiTime(): LocalTime = RHMIDateUtils.convertFromRhmiTime(this)
+data class CalendarStateViewModel(
+	val date: LocalDate,
+	val events: List<CalendarEvent>,
+	val onAction: suspend (event: CalendarEvent?) -> Unit,
+)
+
+fun RHMIComponent.CalendarDay.viewModel(actionHandler: suspend (RHMIAction?, extra: Map<Int, Any>?) -> Unit): CalendarStateViewModel {
+	val appointments = this.getAppointmentListModel()?.asRaListModel()?.value ?: RHMIModel.RaListModel.RHMIListConcrete(4)
+	val events = appointments.getWindow(0, appointments.endIndex).map {
+		val start = (it.getOrNull(0)?.asEtchInt() ?: 0).fromRhmiTime()
+		val end = (it.getOrNull(1)?.asEtchInt() ?: 0).fromRhmiTime()
+		// iconId isn't used yet
+		val title = (it.getOrNull(3) as? String) ?: "null"
+		CalendarEvent(start, end, title)
+	}
+	return CalendarStateViewModel(
+		date = this.getDateModel()?.asRaIntModel()?.value?.fromRhmiDate() ?: LocalDate(1900, 1, 1),
+		events = events
+	) {
+		actionHandler(this.getAction(), mapOf(0 to events.indexOf(it) + 1))
+	}
+}
+
+@Composable
+fun RHMICalendarState(modifier: Modifier, state: CalendarStateViewModel) {
+	val scope = rememberCoroutineScope()
+
+	Column(modifier = modifier) {
+		val title = LocalDate.Format {
+			year()
+			char('-')
+			monthNumber()
+			char('-')
+			dayOfMonth()
+		}.format(state.date)
+		Text(title,
+			style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.primary,
+			modifier = Modifier.padding(4.dp).clickable { scope.launch {
+				state.onAction(null)
+			}}
+		)
+		CalendarDayView(
+			events = state.events
+		) {
+			scope.launch {
+				state.onAction(it)
+			}
+		}
+	}
+}
